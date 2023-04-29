8badf54092b802f86f73efbd95e1b056eae1ddae
Consultant <christopher.tamayocazarez@gmail.com>
2023-04-29 12:34:25 -0400

    Hi what are you looking for ?

--- code-base-sdk/src/main/java/com/tamayo/code_base_sdk/utils/UIState.kt
+++ code-base-sdk/src/main/java/com/tamayo/code_base_sdk/utils/UIState.kt
@@ -1,7 +1,25 @@
 package com.tamayo.code_base_sdk.utils
 
-sealed class UIState<out T>{
+/**
+ * Represents the various states of a UI action that involves a result of type T.
+ * @param T the type of data returned upon successful completion of the action
+ */
+sealed class UIState<out T> {
+
+    /**
+     * Represents the loading state of the UI action.
+     */
     object LOADING : UIState<Nothing>()
-    data class SUCCESS<T>(val data: T): UIState<T>()
-    data class ERROR(val ERROR: Exception): UIState<Nothing>()
+
+    /**
+     * Represents the successful state of the UI action.
+     * @param data the data returned upon successful completion of the action
+     */
+    data class SUCCESS<T>(val data: T) : UIState<T>()
+
+    /**
+     * Represents the error state of the UI action.
+     * @param ERROR the error that occurred during the action
+     */
+    data class ERROR(val ERROR: Exception) : UIState<Nothing>()
 }
