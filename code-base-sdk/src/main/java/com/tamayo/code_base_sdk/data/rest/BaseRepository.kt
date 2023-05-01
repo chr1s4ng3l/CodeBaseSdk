90f8a94c5a8a5d6c2ee5f1448eeb7e2fc526710f
Consultant <christopher.tamayocazarez@gmail.com>
2023-05-01 13:49:01 -0400

    BaseRepository

--- code-base-sdk/src/main/java/com/tamayo/code_base_sdk/data/rest/BaseRepository.kt
+++ code-base-sdk/src/main/java/com/tamayo/code_base_sdk/data/rest/BaseRepository.kt
@@ -1,31 +1,46 @@
-package com.tamayo.code_base_sdk.rest
+package com.tamayo.code_base_sdk.data.rest
 
 import android.util.Log
 import com.tamayo.code_base_sdk.domain.DomainCharacter
 import com.tamayo.code_base_sdk.domain.mapToDomain
 import com.tamayo.code_base_sdk.utils.UIState
-import com.tamayo.code_base_sdk.utils.CharactersType
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
+/**
+ * This interface provides a method to get the characters by making a request to
+ * the API using the characterType and returning a flow of [UIState].
+ */
 interface BaseRepository {
+
+    /**
+     * This function fetches the characters from the API and returns a flow of [UIState] as a result.
+     * @param charactersType The type of character to be fetched from the API.
+     * @return A [Flow] of [UIState] which contains the list of [DomainCharacter] on [UIState.SUCCESS],
+     * [UIState.LOADING] when request is being made and [UIState.ERROR] on error.
+     */
     fun getCharacters(charactersType: String): Flow<UIState<List<DomainCharacter>>>
 }
 
+/**
+ * This class implements the [BaseRepository] interface and provides a method to fetch characters from the API.
+ * @param service An instance of [BaseService] to make the network call.
+ * @param ioDispatcher A dispatcher for running the coroutine on IO thread.
+ */
 class BaseRepositoryImpl @Inject constructor(
     private val service: BaseService,
     private val ioDispatcher: CoroutineDispatcher
 ) : BaseRepository {
+
     override fun getCharacters(charactersType: String): Flow<UIState<List<DomainCharacter>>> = flow {
         emit(UIState.LOADING)
         try {
             val response = service.getCharacters(charactersType)
             if (response.isSuccessful){
                 response.body()?.let {
-                    Log.d("TAG", "getCharacters: $it")
                     emit(UIState.SUCCESS(it.relatedTopics.mapToDomain()))
                 } ?: throw Exception("Response was null")
 
